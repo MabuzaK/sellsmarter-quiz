ce97ae36108ec952873f3e76dd085a4b8b8dfa8b
MabuzaK <101397349+MabuzaK@users.noreply.github.com>
2022-05-17 11:15:19 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const frontController = require('../controllers/userController');
+
+
+
+router.get("/", frontController.home)
+
+router.get("/Questionnaire", frontController.question)
+
+router.get("/successPage", frontController.suc)
+
+
+router.post('/details/save', frontController.add_answers)
+
+router.post('/user/saved', frontController.add_user)
+
+// router.post('Questionnaire/complete', frontController.add_Questionnaire)
+
+
+module.exports = router
